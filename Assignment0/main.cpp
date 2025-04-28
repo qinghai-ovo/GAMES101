a31c6a18a3d060861601c06cf09b9bddd53f85ce
qinghai <qinghai1027@gmail.com>
2025-04-28 20:28:23 +0900

    Assgnment0 commit

---
+++ Assignment0/main.cpp
@@ -0,0 +1,72 @@
+#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+#include<iostream>
+
+int main(){
+
+    // Basic Example of cpp
+    std::cout << "*****Example of cpp***** \n";
+    float a = 1.0, b = 2.0;
+    std::cout << a << std::endl;
+    std::cout << a/b << std::endl;
+    std::cout << std::sqrt(b) << std::endl;
+    std::cout << std::acos(-1) << std::endl;
+    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+
+    // Example of vector
+    std::cout << "*****Example of vector***** \n";
+    // vector definition
+    Eigen::Vector3f v(1.0f,2.0f,3.0f);
+    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    // vector output
+    std::cout << "Example of output \n";
+    std::cout << v << std::endl;
+    // vector add
+    std::cout << "Example of add \n";
+    std::cout << v + w << std::endl;
+    // vector scalar multiply
+    std::cout << "Example of scalar multiply \n";
+    std::cout << v * 3.0f << std::endl;
+    std::cout << 2.0f * v << std::endl;
+
+    // Example of matrix
+    std::cout << "*****Example of matrix***** \n";
+    // matrix definition
+    Eigen::Matrix3f i,j;
+    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
+    // matrix output
+    std::cout << "Example of output \n";
+    std::cout << i << std::endl;
+    // matrix add i + j
+    std::cout << i + j << std::endl;
+    // matrix scalar multiply i * 2.0
+    std::cout << i * 2.0f << std::endl;
+    // matrix multiply i * j
+    std::cout << i * j << std::endl; 
+    // matrix multiply vector i * v
+    std::cout << i * v <<std::endl;
+
+    std::cout<<"*****Transformation*****"<<std::endl;
+    /* Given point p(2,1), first rotate p around the origin by 45 degrees 
+       and then translate it using the vector (1,2). Use Homogeneous coordinates. */
+    Eigen::Vector3f p(2.0f, 1.0f, 1.0f);
+    Eigen::Matrix3f rotation,translation;
+    
+    float theta = 45.0f * M_PI / 180.0f;
+
+    rotation << 
+        cos(theta), -sin(theta), 0.0f, 
+        sin(theta), cos(theta), 0.0f, 
+        0.0f, 0.0f, 1.0f;
+    
+    translation << 
+        1.0f, 0.0f, 1.0f, 
+        0.0f, 1.0f, 2.0f, 
+        0.0f, 0.0f, 1.0f;
+
+    std::cout<< translation * rotation * p << std::endl;
+
+    return 0;
+}
